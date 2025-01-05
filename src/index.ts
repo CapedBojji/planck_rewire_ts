8161ee9839b0d9542b22dfa7193bf24977bd7475
CapedBaldy <capedbaldy141@gmail.com>
2025-01-05 21:04:08 +0000

    refactor: enhance module management in PlanckRewirePlugin for better hot reloading

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,28 @@ interface ModuleInfo {
 class PlanckRewirePlugin implements Plugin {
 	private readonly folders: Folder[];
 	private readonly moduleToInfo: Map<ModuleScript, ModuleInfo>;
-	private currentModule: ModuleScript | undefined;
+	private currentModule: { old: ModuleScript, new: ModuleScript } | undefined;
 	private readonly hotReloader = new HotReloader();
 
 	constructor(folders: Folder[]) {
 		this.folders = folders;
 		this.moduleToInfo = new Map();
-
 	}
+
+	private cleanupModule(module: ModuleScript) {
+		const moduleInfo = this.moduleToInfo.get(module);
+		if (moduleInfo === undefined) return;
+		moduleInfo.nameToSystem.forEach((system, name) => {
+			// Remove the system
+			this.moduleToInfo.forEach((moduleInfo) => {
+				if (moduleInfo.systemToName.has(system)) {
+					moduleInfo.systemToName.delete(system);
+					moduleInfo.nameToSystem.delete(name);
+				}
+			})
+		})
+	}
+
 
 	build(schedular: Scheduler<unknown[]>): void {
 		schedular._addHook(schedular.Hooks.SystemAdd, (info) => {
@@ -24,24 +38,25 @@ class PlanckRewirePlugin implements Plugin {
 				return;
 			}
 			const systemInfo = info.system;
-			const moduleInfo = this.moduleToInfo.get(this.currentModule);
-			if (moduleInfo === undefined) {
+			const currentSystemName = systemInfo.name;
+			const isReloading = this.currentModule.old !== this.currentModule.new;
+			if (isReloading) {
+				const oldModuleInfo = this.moduleToInfo.get(this.currentModule.old);
+				assert(oldModuleInfo !== undefined, "Old module info is undefined"); // This should never happen
+				// Check if the new system is in the old module
+				// If it is	, remove the old system
+				if (oldModuleInfo.nameToSystem.has(currentSystemName)) {
+					const oldSystem = oldModuleInfo.nameToSystem.get(currentSystemName)!;
+					schedular.removeSystem(oldSystem);
+				}
+			}
+			// Add the system to the module
+			if (!this.moduleToInfo.has(this.currentModule.new)) {
 				const nameToSystem = new Map<string, SystemFn<unknown[]>>();
 				const systemToName = new Map<SystemFn<unknown[]>, string>();
 				nameToSystem.set(systemInfo.name, systemInfo.system);
 				systemToName.set(systemInfo.system, systemInfo.name);
-				this.moduleToInfo.set(this.currentModule, { nameToSystem, systemToName });
-				return;
-			}
-			if (moduleInfo.nameToSystem.has(systemInfo.name)) {
-				// Remove the current system
-				schedular.removeSystem(systemInfo.system);
-				// Replace the old system with the new system
-				schedular.replaceSystem(moduleInfo.nameToSystem.get(systemInfo.name)!, systemInfo.system);
-			} else {
-				// Add the new system
-				moduleInfo.nameToSystem.set(systemInfo.name, systemInfo.system);
-				moduleInfo.systemToName.set(systemInfo.system, systemInfo.name);
+				this.moduleToInfo.set(this.currentModule.new, { nameToSystem, systemToName });
 			}
 		})
 		schedular._addHook(schedular.Hooks.SystemRemove, (info) => {
@@ -60,29 +75,29 @@ class PlanckRewirePlugin implements Plugin {
 			// Find the module that the old system is in
 			this.moduleToInfo.forEach((moduleInfo, module) => {
 				if (moduleInfo.systemToName.has(oldSystemInfo.system)) {
-					const oldName = moduleInfo.systemToName.get(oldSystemInfo.system)!;
+					const oldName = oldSystemInfo.name;
+					const newName = newSystemInfo.name;
 					// Delete the old system
 					moduleInfo.systemToName.delete(oldSystemInfo.system);
+					moduleInfo.nameToSystem.delete(oldName);
 					// Add the new system with the old name
-					moduleInfo.systemToName.set(newSystemInfo.system, oldName);
-					// Replace the old system with the new system
-					moduleInfo.nameToSystem.set(oldName, newSystemInfo.system);
+					moduleInfo.systemToName.set(newSystemInfo.system, newName);
+					moduleInfo.nameToSystem.set(newName, newSystemInfo.system);
 				}
 			})
 		})
 		// Load all the modules
 		this.folders.forEach(folder => {
-			folder.GetChildren().forEach(child => {
-				if (child.IsA("ModuleScript")) {
-					this.hotReloader.listen(child, (module: ModuleScript, context: Context) => {
-						// Set the current module
-						this.currentModule = module;
-						pcall(() => { require(module) });
-						this.currentModule = undefined;
-					}, (module: ModuleScript, context: Context) => {
-
-					})
-				}
+			this.hotReloader.scan(folder, (module: ModuleScript, context: Context) => {
+				// Set the current module
+				this.currentModule = { old: context.originalModule, new: module };
+				pcall(() => { require(module) });
+				this.currentModule = undefined;
+				if (context.isReloading)
+					this.cleanupModule(context.originalModule);
+			}, (module: ModuleScript, context: Context) => {
+				if (context.isReloading) return;
+				this.cleanupModule(module);
 			})
 		})
 	}
